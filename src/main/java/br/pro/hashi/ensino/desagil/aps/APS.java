68e56adecd2a4ae5c97c2e13dfa8462110340a71
Marcos Vinícius <marquinhosvinis@gmail.com>
2020-06-30 08:37:21 -0300

    Alterações APS.java

--- src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
@@ -8,25 +8,16 @@ import java.util.LinkedList;
 
 public class APS {
     public static void main(String[] args) {
-
-        // Constrói o modelo, que é simplesmente uma lista
-        // de calculadoras. Aqui a coesão está boa: para
-        // criar uma nova calculadora, basta criar a
-        // respectiva classe e adicionar uma linha abaixo.
         LinkedList<Gate> model = new LinkedList<>();
         model.add(new NotGate());
         model.add(new AndGate());
         model.add(new OrGate());
         model.add(new NandGate());
         model.add(new XorGate());
+        model.add(new MuxGate());
 
-        // Constrói a visão, que também é um controlador.
-        // Juntar visão e controlador prejudica a coesão,
-        // mas decidir fazê-lo para simplificar o código.
         View view = new View(model);
 
-        // Todas as linhas abaixo já foram explicadas nos Desafios.
-        // Além disso, não é importante entendê-las para estas APS.
         javax.swing.SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame();
             frame.setContentPane(view);
